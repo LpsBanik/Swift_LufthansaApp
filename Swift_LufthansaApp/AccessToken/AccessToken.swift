dff55f8abfe8ebcd7ef5194e0db38e09f24a53ca
LpsBanik <banik.lps@gmail.com>
2017-12-11 18:05:18 +0300

    data part 1 and server manager

---
+++ Swift_LufthansaApp/AccessToken/AccessToken.swift
@@ -0,0 +1,13 @@
+//
+//  AccessToken.swift
+//  Swift_LufthansaApp
+//
+//  Created by Сергей Кохан on 11.12.2017.
+//  Copyright © 2017 Siarhei Kokhan. All rights reserved.
+//
+
+import UIKit
+
+class AccessToken: NSObject {
+
+}
---
+++ Swift_LufthansaApp/Managers/ServerManager.swift
@@ -0,0 +1,13 @@
+//
+//  ServerManager.swift
+//  Swift_LufthansaApp
+//
+//  Created by Сергей Кохан on 11.12.2017.
+//  Copyright © 2017 Siarhei Kokhan. All rights reserved.
+//
+
+import UIKit
+
+class ServerManager: NSObject {
+
+}
